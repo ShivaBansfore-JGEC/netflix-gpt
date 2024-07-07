d676283b50ab4110f2614413d83f8a3148e00f2b
ShivaBansfore-JGEC <shivabansfore123@gmail.com>
2024-07-07 10:27:36 +0530

    feat: signIn/signUp form and login page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Body from './components/Body';
+import Browse from './components/Browse';
+import Login from './components/Login';
 
 function App() {
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Body />
+    },
+    {
+      path: "/browse",
+      element: <Browse />
+    },
+    {
+      path: "/login",
+      element: <Login />
+    }
+  ])
+
   return (
-    <div className="App">
-      Welcome
-    </div>
+    <RouterProvider router={appRouter} />
   );
 }
 
